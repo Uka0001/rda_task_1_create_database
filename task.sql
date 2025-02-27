c1e0eb6d35c2c86db1f8ea1fecd051f52bf3d101
oleksii.b <oleksii.b@dev.tagsoft.co>
2025-02-27 22:52:25 +0200

    resolved task

--- task.sql
+++ task.sql
@@ -1,1 +1,38 @@
-# Write your SQL code for the database creation here. Good luck! 
+CREATE DATABASE ShopDB;
+
+USE ShopDB;
+
+CREATE TABLE Products
+(
+    ID              INT PRIMARY KEY AUTO_INCREMENT,
+    Name            VARCHAR(50)    NOT NULL,
+    Description     VARCHAR(100),
+    Price           DECIMAL(10, 2) NOT NULL,
+    WarehouseAmount INT            NOT NULL
+);
+
+CREATE TABLE Customers
+(
+    ID        INT PRIMARY KEY AUTO_INCREMENT,
+    FirstName VARCHAR(50) NOT NULL,
+    LastName  VARCHAR(50) NOT NULL,
+    Email     VARCHAR(50) NOT NULL,
+    Address   TEXT
+);
+
+CREATE TABLE Orders
+(
+    ID         INT PRIMARY KEY AUTO_INCREMENT,
+    CustomerID INT,
+    Date       DATE NOT NULL,
+    FOREIGN KEY (CustomerID) REFERENCES Customers (ID) ON DELETE SET NULL
+);
+
+CREATE TABLE OrderItems
+(
+    ID        INT PRIMARY KEY AUTO_INCREMENT,
+    OrderID   INT,
+    ProductID INT,
+    FOREIGN KEY (OrderID) REFERENCES Orders (ID) ON DELETE SET NULL,
+    FOREIGN KEY (ProductID) REFERENCES Products (ID) ON DELETE SET NULL
+);
